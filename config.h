c9ff0c6814570f7d4cad312318f07006602426a4
Leandro Moreira <leandromoreiravaz@gmail.com>
2021-11-10 11:34:01 +0000

    7. Patch focus on click

--- config.h
+++ config.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 1;        /* 0 means no focus on scrolling */
 static const char *fonts[]          = { "Terminus:size=10", "Font Awesome 5 Free Regular:style=Regular:size=12" };
 static const char dmenufont[]       = "Terminus:size=10";
 static const char col_gray1[]       = "#222222";
